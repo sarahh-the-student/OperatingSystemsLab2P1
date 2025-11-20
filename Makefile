f8608642c564fefa560a2cd9950852be4189e1e1
Sarah Cole <sarah.cole@bison.howard.edu>
2025-11-20 20:44:49 +0000

    cleaned up code given feedback

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
+
+all: prog1 prog2 my3proc
+CFLAGS = -Wall -g
+
 prog1: main.c
-	gcc main.c -o prog1
+	gcc $(CFLAGS) main.c -o prog1
 
 prog2: main2.c
-	gcc main2.c -o prog2
+	gcc $(CFLAGS) main2.c -o prog2
 
 my3proc: main3.c
-	gcc main3.c -o my3proc
+	gcc $(CFLAGS) main3.c -o my3proc
+
+clean:
+	rm -f prog1 prog2 my3proc
--- main3.c
+++ main3.c
@@ -1,40 +1,67 @@
+/*
+ * Program Description: 
+ * This program creates two child processes. Each child loops a random number 
+ * of times (up to 30), sleeping for a random duration (1-10s) in each iteration.
+ * * The parent waits for both children to complete and decodes their exit status
+ * using WIFEXITED/WEXITSTATUS to ensure valid termination reporting.
+ * * Randomness is seeded using time(NULL) ^ PID to ensure unique sequences per child.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>   
+#include <sys/wait.h> 
+#include <time.h>    
 
 void child_process();
+
 // parent process
-int main (){
+int main () {
   int pid;
   int i; 
-  for (i = 0; i<2; i++){
-    pid = fork(); // stores two child pids
-    if (pid == 0) 
-				child_process();
-    else if (pid<0) 
-				exit(1);
+
+  // Loop to create two child processes
+  for (i = 0; i < 2; i++) {
+    pid = fork(); 
+    if (pid == 0) {
+      child_process();
+    }
+    else if (pid < 0) {
+      perror("Fork failed");
+      exit(1);
+    }
   }
 
-  for (i=0; i < 2; i++){
+  // Parent continues the loop to wait for children
+  for (i = 0; i < 2; i++){
     int status;
     int completed_pid = wait(&status);
-    printf("Child Pid: %d has completed with exit status: %d\n", completed_pid, status);
-  }
-}
+    if (WIFEXITED(status)) {
+      printf("Parent: Child Pid %d has completed with exit status: %d\n", completed_pid, WEXITSTATUS(status));
+    }
+  } 
+  return 0;
+} 
 
 // run one child process
 void child_process(){
   int pid = getpid();
   int parent_pid = getppid();
-  srandom(pid % 42); // seeding random number
-  int n = 1 + (random() % 30); // random number no more than 30
+  srandom(time(NULL) ^ pid);
+
+  // random number no more than 30 
+  int n = 1 + (random() % 30); 
   int i;
-  for (i=0; i < 2; i++){
-    int time = 1 + (random() % 10); // sleep time no more than 10 seconds
-    printf("Child Pid: %d is going to sleep for %d seconds!\n", pid, time);
-    sleep(time); // sleep
-    printf("Child Pid: %d is awake!\nWhere is my Parent: %d?\n", pid, parent_pid); // print if awake
+  printf("Child Pid: %d starting %d iterations...\n", pid, n);
+  fflush(stdout);
+
+  for (i = 0; i < n; i++){
+    int sleep_time = 1 + (random() % 10);
+    printf("Child Pid: %d is going to sleep for %d seconds!\n", pid, sleep_time);
+    fflush(stdout);
+    sleep(sleep_time);
+    printf("Child Pid: %d is awake! Where is my Parent: %d?\n", pid, parent_pid); 
+    fflush(stdout);
   }
   exit(0);
-}
-
-
+} 
